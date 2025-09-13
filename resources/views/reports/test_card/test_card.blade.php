b040ce7b484214b6c7f8452d8be48196721c6d80
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2025-09-13 12:21:46 +0300

    تاريخ الميلاد

--- resources/views/reports/test_card/test_card.blade.php
+++ resources/views/reports/test_card/test_card.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>استمارة اختبار ({{ $lead->name }})</title>
+    <title>استمارة اختبار</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
