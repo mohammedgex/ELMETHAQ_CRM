6b01d8f6a0332c50732866d6cc3657f4144bfa15
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2025-09-24 16:34:16 +0300

    تاريخ الميلاد

--- resources/views/tests/customers-test.blade.php
+++ resources/views/tests/customers-test.blade.php
@@ -294,7 +294,6 @@
             </div>
         @endauth
 
-
         <!-- Loading Overlay -->
         <div id="loading-overlay"
             style="display: none; position: fixed; z-index: 9999; top:0; left:0; width:100%; height:100%; background: rgba(255,255,255,0.8);">
