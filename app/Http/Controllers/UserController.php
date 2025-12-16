5698cf34a37ddcd85ec5da1093f12be389f508ed
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2025-12-16 13:18:25 +0200

    gemini

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     public function index()
     {
         # code...
-        $users = User::where('id', '!=', auth()->user()->id)->get();
+        $users = User::all();
         return view('users.users', [
             'users' => $users
         ]);
