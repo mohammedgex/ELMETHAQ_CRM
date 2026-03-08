1093152167ee0bd0a6d9dd93485382e68ee15e65
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2026-03-08 12:45:43 +0200

    bag and

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -108,25 +108,25 @@ class AppServiceProvider extends ServiceProvider
                 ];
             }
             // العملاء المحتملون
-            if ($user->role === 'admin' || $user->permissions->contains('permission', 'loginFail-access')) {
-                $existingMenu[] = [
-                    'text' => 'محاولات دخول فاشلة',
-                    'url' => 'admin/login-fail',
-                    'icon' => 'fas fa-user-secret',
-                ];
-            }
+            // if ($user->role === 'admin' || $user->permissions->contains('permission', 'loginFail-access')) {
+            //     $existingMenu[] = [
+            //         'text' => 'محاولات دخول فاشلة',
+            //         'url' => 'admin/login-fail',
+            //         'icon' => 'fas fa-user-secret',
+            //     ];
+            // }
             $pendingCount = ModelsDocumentType::where('order_status', 'panding')->count();
 
             // العملاء المحتملون
-            if ($user->role === 'admin' || $user->permissions->contains('permission', 'requests-show')) {
-                $existingMenu[] = [
-                    'text'  => 'طلبات الملفات',
-                    'url'   => 'admin/document-requests', // غيره حسب مسار صفحة الطلبات
-                    'icon'  => 'fas fa-file-alt',
-                    'label' => $pendingCount,
-                    'label_color' => 'warning', // success, danger, info, primary...
-                ];
-            }
+            // if ($user->role === 'admin' || $user->permissions->contains('permission', 'requests-show')) {
+            //     $existingMenu[] = [
+            //         'text'  => 'طلبات الملفات',
+            //         'url'   => 'admin/document-requests', // غيره حسب مسار صفحة الطلبات
+            //         'icon'  => 'fas fa-file-alt',
+            //         'label' => $pendingCount,
+            //         'label_color' => 'warning', // success, danger, info, primary...
+            //     ];
+            // }
 
             // العملاء
             if ($user->role === 'admin' || $user->permissions->contains('permission', 'customers-show')) {
