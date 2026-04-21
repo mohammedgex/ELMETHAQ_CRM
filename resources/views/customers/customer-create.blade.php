bfdc81667a5ddf8b5bd728a89311aecd04051db9
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2026-04-21 12:21:45 +0200

    logo tab

--- resources/views/customers/customer-create.blade.php
+++ resources/views/customers/customer-create.blade.php
@@ -2022,7 +2022,7 @@
                 }
 
                 const model = genAI.getGenerativeModel({
-                    model: "gemini-2.0-flash",
+                    model: "gemini-2.5-flash",
                 });
 
                 const prompt = `
